bdb95f0dc04f933804f7bf532996aabccbdefa16
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-11-10 00:11:34 +0900

    feat: top page.

--- apps/json-server-next/src/app/page/top/top.module.ts
+++ apps/json-server-next/src/app/page/top/top.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopComponent } from './component/top.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TopComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [TopComponent],
 })
 export class TopModule {}
