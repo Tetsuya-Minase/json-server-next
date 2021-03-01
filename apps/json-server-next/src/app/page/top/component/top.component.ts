983eac39c56cc1927816bd2764a5ef635a0147d2
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-02 00:37:15 +0900

    refactor: use text-link module in top page.

--- apps/json-server-next/src/app/page/top/component/top.component.ts
+++ apps/json-server-next/src/app/page/top/component/top.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.scss'],
+  styleUrls: ['./top.component.scss']
 })
-export class TopComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {
-    console.log('top page.');
-  }
+export class TopComponent {
+  readonly links = {
+    edit: {
+      url: '/edit',
+      text: '登録ページ'
+    },
+    list: {
+      url: '/list',
+      text: '登録内容一覧'
+    }
+  } as const;
 }
--- apps/json-server-next/src/app/page/top/top.module.ts
+++ apps/json-server-next/src/app/page/top/top.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopComponent } from './component/top.component';
 import { RouterModule } from '@angular/router';
+import { TextLinkModule } from '../../common/atoms/text-link/text-link.module';
 
 @NgModule({
   declarations: [TopComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [TopComponent],
+  imports: [CommonModule, RouterModule, TextLinkModule],
+  exports: [TopComponent]
 })
-export class TopModule {}
+export class TopModule {
+}
