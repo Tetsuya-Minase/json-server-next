faab233532f36991a04d5d8aa41d127601d97717
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-02 00:36:53 +0900

    refactor: text-link module.

--- apps/json-server-next/src/app/common/atoms/text-link/text-link.component.ts
+++ apps/json-server-next/src/app/common/atoms/text-link/text-link.component.ts
@@ -2,10 +2,10 @@ import { Component, Input } from '@angular/core';
 import { LinkValue } from '../../../model/objects/LinkValue';
 
 @Component({
-  selector: 'atom-router-link',
-  templateUrl: './router-link.component.html',
-  styleUrls: ['./router-link.component.scss'],
+  selector: 'text-link',
+  templateUrl: './text-link.component.html',
+  styleUrls: ['./text-link.component.scss']
 })
-export class RouterLinkComponent {
+export class TextLinkComponent {
   @Input() linkObject: LinkValue;
 }
--- apps/json-server-next/src/app/common/atoms/text-link/text-link.module.ts
+++ apps/json-server-next/src/app/common/atoms/text-link/text-link.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterLinkComponent } from './router-link.component';
+import { TextLinkComponent } from './text-link.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  exports: [RouterLinkComponent],
-  declarations: [RouterLinkComponent]
+  exports: [TextLinkComponent],
+  declarations: [TextLinkComponent]
 })
-export class RouterLinkModule {
+export class TextLinkModule {
 }
